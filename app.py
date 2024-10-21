c45ad6e90507105bbb25a8616be0bb967eaaf491
SlurpyMK3 <axel.muhlheim@gmail.com>
2024-10-21 15:42:45 +0200

    test2

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/")from flask import Flask
+
+
+from flask import current_app, g
+app = Flask(__name__)
+
 @app.route("/")
+def home():
+    return "Hello,oui!"
+
+
+
+
 def home():
     return "Hello, Flask !"
