743317a9d3ba490d8854d5aa57237210c57a7468
DrinePsylook <nmohaouzine.sandrine@gmail.com>
2024-10-22 12:31:46 +0200

    récupération des données

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+from flaskr.db import read_db
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask !"
+
+@app.route("/simple-page")
+def simple_page():
+    fruits = ['orange', 'banane', 'fraise']
+    return render_template('simple_page.html', fruits=fruits)
+
+@app.route('/table/<name>')
+def sql_table(name):
+    db_con = sqlite3.connect("flaskr/data.db")
+    cur = db_con.cursor()
+    sql_request = f"SELECT * FROM {name} LIMIT 50"
+    res = cur.execute(sql_request)
+    resultat = res.fetchall()
+    return resultat
