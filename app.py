561dac04f5f82960f0171624361a186d0efd3f7f
SlurpyMK3 <axel.muhlheim@gmail.com>
2024-10-22 09:07:29 +0200

    add db.py / __init__.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, 
 
-@app.route("/")from flask import Flask
-
-
-from flask import current_app, g
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello,oui!"
 
-
-
-
-def home():
-    return "Hello, Flask !"
+ 
