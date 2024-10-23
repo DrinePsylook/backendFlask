94d7cb458abe718c012a9ef5a4e42e61116cb249
DrinePsylook <nmohaouzine.sandrine@gmail.com>
2024-10-23 15:23:29 +0200

    projet commun

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask !"
+    return render_template('index.html')
+
+@app.route('/table/<name>')
+def sql_table(name):
+    db_con = sqlite3.connect("flaskr/data.db")
+    cur = db_con.cursor()
+    
+    if name == "all":
+        sql_request = '''SELECT customer.id AS id_client, customer.country AS pays, co.id AS id_commande_client, co.invoice_nb AS numero_facture, co.invoice_date AS date_facture, od.id AS id_detail_commande, od.quantity AS quantite, product.id AS id_produit, product.description AS description, product.price As prix     
+                            FROM customer 
+                            LEFT JOIN customer_order as co
+                                ON customer.id = co.customer_id
+                            LEFT JOIN order_detail as od
+                                ON co.id = od.order_id
+                            LEFT JOIN product
+                                ON od.product_id = product.id
+                            ORDER BY customer.id
+                            LIMIT 50'''
+    else:
+        sql_request = f"SELECT * FROM {name} LIMIT 50"
+
+    res = cur.execute(sql_request)
+    resultat = res.fetchall()
+
+    columns_name = [description[0] for description in cur.description]
+    return render_template('tables_template.html', tables=resultat, name=name, col = columns_name)
